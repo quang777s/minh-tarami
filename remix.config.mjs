e709d65bfb557806009247417a8e7f6ff86e903e
Quang Pham <quang@taramind.xyz>
2025-05-19 08:39:54 +0000

    Update

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,12 +1,3 @@
-// import pkg from '@remix-run/dev';
-// const { defineConfig } = pkg;
-// export default defineConfig({
-//   ignoredRouteFiles: ["**/*.css"],
-//   tailwind: true,
-//   postcss: true,
-// });
-
-// New code directly exporting the config object
 export default {
   ignoredRouteFiles: ["**/*.css"],
   tailwind: true,
