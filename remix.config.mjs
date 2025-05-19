4b818f8259d5385ecc4c27aff950a19ed61f0b55
Quang Pham <quang@taramind.xyz>
2025-05-19 08:33:33 +0000

    Update config

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
+import pkg from '@remix-run/dev';
+const { defineConfig } = pkg;
+
+export default defineConfig({
   ignoredRouteFiles: ["**/*.css"],
   tailwind: true,
   postcss: true,
-};
+});
